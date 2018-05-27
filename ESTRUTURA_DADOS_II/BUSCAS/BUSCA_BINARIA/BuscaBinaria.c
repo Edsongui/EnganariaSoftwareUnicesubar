fc237b5826b13b8f81f63c06235b119856d05610
EdsonGui <edson-guido@hotmail.com>
2018-05-27 16:40:18 -0300

    Adicionado código busca binária

---
+++ ESTRUTURA_DADOS_II/BUSCAS/BUSCA_BINARIA/BuscaBinaria.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int buscaBinaria(int *vetor, int tamanhoVetor, int elementoBuscado){
+	int i, inicio, meio, fim;
+	inicio = 0;
+	fim = tamanhoVetor-1;
+	
+	while (elementoBuscado<= fim){
+		meio = (inicio+fim)/2;
+		if (elementoBuscado<vetor[meio])
+			fim = meio--;
+		else
+			if(elementoBuscado>vetor[meio])
+				inicio = meio++;
+			else
+				return meio;
+	}
+	return -1;
+}
+
+int main(int argc, char *argv[]) {
+	int tamanhoVetor, i, elementoBuscado, retorno;
+	
+	printf("Informe o tamanho do vetor: ");
+	scanf("%d", &tamanhoVetor);
+	
+	int vetor[tamanhoVetor];
+	
+	for (i=0; i<tamanhoVetor; i++) {
+		printf("Informe o numero para posicao %d: ", i);
+		scanf("%d", &vetor[i]);
+		printf("\n");
+	}
+	
+	printf("Informe o numero para ser encontrado: ");
+	scanf("%d", &elementoBuscado);
+	printf("\n");
+	retorno = buscaBinaria(vetor, tamanhoVetor, elementoBuscado);
+	
+	if (retorno != -1) {
+		printf("Elemento encontrado na posicao %d", retorno);
+	} else {
+		printf("Elemento nao encontrado!");
+	}
+	return 0;
+}
